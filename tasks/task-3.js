780ed60ef87a62b26c6b754540f4ada04b49673c
nelli-belovol <nelli199020@gmail.com>
2021-11-08 11:45:47 +0200

    Update task-3.js

--- tasks/task-3.js
+++ tasks/task-3.js
@@ -1,10 +1,11 @@
 const textArray = [
   ["Hello", "world"],
   ["Brad", "came", "to", "dinner", "with", "us"],
-  ["He", "loves", "tacos"]
+  ["He", "loves", "tacos"],
+  ["He", "loves", "tacos"],
 ]
 
-const format = ["LEFT", "RIGHT", "LEFT"];
+const format = ["LEFT", "RIGHT", "LEFT", "RIGHT"];
 
 const limit = 16;
 
@@ -21,18 +22,18 @@ format.map((side, index)=>{
        let secondPart = data[index].join(" "); 
   
         if(side === "LEFT"){
-         rezult.push(`*${firstPart.padEnd(18, " ")}*`);
-         rezult.push(`*${secondPart.padEnd(18, " ")}*`)
+         rezult.push(`*${firstPart.padEnd(16, " ")}*`);
+         rezult.push(`*${secondPart.padEnd(16, " ")}*`)
        }else{
-         rezult.push(`*${firstPart.padStart(18, " ")}*`);
-         rezult.push(`*${secondPart.padStart(18, " ")}*`)
+         rezult.push(`*${firstPart.padStart(16, " ")}*`);
+         rezult.push(`*${secondPart.padStart(16, " ")}*`)
        }
   } else {
     if(side === "LEFT"){  
-        message = `*${data[index].join(' ').padEnd(18, " ")}*`;
+        message = `*${data[index].join(' ').padEnd(16, " ")}*`;
         rezult.push(message);
       } else {
-          const message = `*${data[index].join(' ').padStart(18, " ")}*`;   
+          const message = `*${data[index].join(' ').padStart(16, " ")}*`;   
           rezult.push(message);
         }
     
